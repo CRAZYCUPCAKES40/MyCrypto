2e025be42e91a0c8913960599256a5f87102abeb
Matej Lubej <matej.lubej@poviolabs.com>
2020-04-23 18:53:09 +0200

    Add multiple sort column support for table

--- common/v2/features/Settings/components/AddressBook.tsx
+++ common/v2/features/Settings/components/AddressBook.tsx
@@ -88,13 +88,13 @@ export default function AddressBook({
     isNumber(deletingIndex) ? [deletingIndex] : [],
     [...undoDeletingIndexes]
   ];
-  const overlayRowsFlat = [...overlayRows[0], ...overlayRows[1].map(row => row[0])];
+  const overlayRowsFlat = [...overlayRows[0], ...overlayRows[1].map((row) => row[0])];
 
   const getDisplayAddressBook = (): ExtendedAddressBook[] => {
     const accountsTemp = cloneDeep(addressBook);
     overlayRows[1]
       .sort((a, b) => a[0] - b[0])
-      .forEach(index => {
+      .forEach((index) => {
         accountsTemp.splice(index[0], 0, addressBookRestore[index[1]] as ExtendedAddressBook);
       });
     return accountsTemp.sort((a, b) => a.uuid.localeCompare(b.uuid));
@@ -117,13 +117,13 @@ export default function AddressBook({
             })}
             deleteAction={() => {
               setDeletingIndex(undefined);
-              setUndoDeletingIndexes(prev => [...prev, [rowIndex, uuid]]);
+              setUndoDeletingIndexes((prev) => [...prev, [rowIndex, uuid]]);
               deleteAddressBooks(uuid);
             }}
             cancelAction={() => setDeletingIndex(undefined)}
           />
         );
-      } else if (overlayRows[1].length && overlayRows[1].map(row => row[0]).includes(rowIndex)) {
+      } else if (overlayRows[1].length && overlayRows[1].map((row) => row[0]).includes(rowIndex)) {
         // Undo delete overlay
         const { uuid, label, address } = displayAddressBook[rowIndex];
 
@@ -136,7 +136,7 @@ export default function AddressBook({
             })}
             restoreAccount={() => {
               restoreDeletedAddressBook(uuid);
-              setUndoDeletingIndexes(prev => prev.filter(i => i[0] !== rowIndex));
+              setUndoDeletingIndexes((prev) => prev.filter((i) => i[0] !== rowIndex));
             }}
           />
         );
@@ -153,7 +153,9 @@ export default function AddressBook({
           <SEditableText
             truncate={true}
             value={label}
-            saveValue={value => updateAddressBooks(uuid, { address, label: value, network, notes })}
+            saveValue={(value) =>
+              updateAddressBooks(uuid, { address, label: value, network, notes })
+            }
           />
         </Label>,
         <EthAddress key={2} address={address} truncate={truncate} isCopyable={true} />,
@@ -164,7 +166,7 @@ export default function AddressBook({
           key={4}
           truncate={true}
           value={notes}
-          saveValue={value => updateAddressBooks(uuid, { address, label, network, notes: value })}
+          saveValue={(value) => updateAddressBooks(uuid, { address, label, network, notes: value })}
         />,
         <DeleteButton key={5} onClick={() => setDeletingIndex(index)} icon="exit" />
       ]
@@ -172,7 +174,7 @@ export default function AddressBook({
     config: {
       primaryColumn: 'Label',
       sortableColumn: overlayRowsFlat.length ? '' : 'Label',
-      sortFunction: (a: any, b: any) => {
+      sortFunction: () => (a: any, b: any) => {
         const aLabel = a.props.label;
         const bLabel = b.props.label;
         return aLabel === bLabel ? true : aLabel.localeCompare(bLabel);
